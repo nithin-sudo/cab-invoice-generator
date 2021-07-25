d9a17dd8dfdbb4fa057568c6ccebf094bec0517a
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-25 16:23:42 +0530

    Added: initial setup for cab invoice generator.

---
+++ src/main/java/com/bridgelabz/cabinvoice/InvoiceGenerator.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.cabinvoice;
+
+public class InvoiceGenerator
+{
+    
+}
---
+++ src/test/java/com/bridgelabz/cabinvoice/InvoiceServiceTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.cabinvoice;
+
+import org.junit.jupiter.api.Test;
+
+public class InvoiceServiceTest
+{
+    @Test
+    public void whenGivenDistanceAndTimeShouldReturnTotalFare()
+    {
+
+    }
+}
