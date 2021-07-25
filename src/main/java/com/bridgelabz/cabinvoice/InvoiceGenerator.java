36e8e98eb2895e204c9b3195555535a7cac24d24
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-25 16:30:25 +0530

    Added: A Method and a TestCase for calculating total fare.

--- src/main/java/com/bridgelabz/cabinvoice/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoice/InvoiceGenerator.java
@@ -1,6 +1,10 @@
 package com.bridgelabz.cabinvoice;
 
-public class InvoiceGenerator
-{
-    
+public class InvoiceGenerator {
+    private static final int COST_PER_MINUTE = 1;
+    private static final double MINIMUM_COSt_PER_KILOMETER = 10;
+
+    public double calculateFare(double distance, int time) {
+        return distance * MINIMUM_COSt_PER_KILOMETER + time * COST_PER_MINUTE;
+    }
 }
--- src/test/java/com/bridgelabz/cabinvoice/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoice/InvoiceServiceTest.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.cabinvoice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class InvoiceServiceTest
-{
+public class InvoiceServiceTest {
     @Test
-    public void whenGivenDistanceAndTimeShouldReturnTotalFare()
-    {
-
+    public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
+        InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = cabInvoiceGenerator.calculateFare(distance, time);
+        System.out.println("Total Fare = " + totalFare);
+        Assertions.assertEquals(25, totalFare, 0.0);
     }
 }
