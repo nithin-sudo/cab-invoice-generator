3980de3472000d70842bc26662eb7483a98702ec
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-25 18:14:32 +0530

    Added: A method and constructor to return enhanced invoice.

---
+++ src/main/java/com/bridgelabz/cabinvoice/InvoiceSummary.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.cabinvoice;
+
+public class InvoiceSummary
+{
+    double avgFare;
+    double totalFare;
+    double numOfRides;
+
+    public InvoiceSummary(int numOfRides, double totalFare) {
+        this.numOfRides = numOfRides;
+        this.totalFare = totalFare;
+        this.avgFare = this.numOfRides / this.totalFare;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary that = (InvoiceSummary) o;
+        return Double.compare(that.avgFare, avgFare) == 0 &&
+                totalFare == that.totalFare &&
+                Double.compare(that.numOfRides, numOfRides) == 0;
+    }
+}
--- src/test/java/com/bridgelabz/cabinvoice/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoice/InvoiceServiceTest.java
@@ -1,12 +1,18 @@
 package com.bridgelabz.cabinvoice;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
+    InvoiceGenerator cabInvoiceGenerator = null;
+    @BeforeEach
+    public void setup() throws Exception
+    {
+        cabInvoiceGenerator = new InvoiceGenerator();
+    }
     @Test
     public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
-        InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double totalFare = cabInvoiceGenerator.calculateFare(distance, time);
@@ -15,21 +21,19 @@ public class InvoiceServiceTest {
     }
     @Test
     public void whenGivenDistanceAndTimeShouldReturnMinimumFare() {
-        InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double totalFare = cabInvoiceGenerator.calculateFare(distance, time);
         System.out.println("Total Fare = " + totalFare);
         Assertions.assertEquals(5, totalFare, 0.0);
     }
-
     @Test
-    public void whenGivenMultipleRidesShouldReturnInvoiceSummary() {
-        InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator();
-        Ride[] rides = { new Ride(2.0, 5),
-                         new Ride(0.1, 1)};
-        double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
-        System.out.println("Total Fare = "+totalFare);
-        Assertions.assertEquals(30, totalFare,0.0);
+    public void givenMultipleRidesShouldReturnInvoiceSummary() {
+            Ride[] rides = {
+                    new Ride(2.0, 5),
+                    new Ride(0.1, 1)};
+            InvoiceSummary summary = cabInvoiceGenerator.calculateFare(rides);
+            InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+            Assertions.assertEquals(expectedInvoiceSummary, summary);
     }
 }
